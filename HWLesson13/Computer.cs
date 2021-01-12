39d2f7b7310ebf2cc39c475c8aea60a5c0c2bf02
MarizzaM <75089445+MarizzaM@users.noreply.github.com>
2021-01-13 01:21:00 +0200

    Add files via upload

---
+++ HWLesson13/Computer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace HWLesson13
+{
+    [DebuggerDisplay("Price {price*3.67} $")]
+    class Computer
+    {
+        public string model;
+        public int price;
+        public int numberOfProcessor;
+        public float screenSize;
+        public bool isTurnOn;
+
+        public int tellMeThePrice()
+        {
+            return price;
+        }
+
+        public float tellMeTheScreenSize()
+        {
+            return screenSize;
+        }
+
+        public void turnOn()
+        {
+            if (isTurnOn) 
+            {
+                Console.WriteLine($"{model} is on");
+            }
+            else
+            {
+                isTurnOn = true;
+                Console.WriteLine($"turning on {model}...");
+            }
+        }
+
+        public void turnOff()
+        {
+            if (!isTurnOn)
+            {
+                Console.WriteLine($"{model} is off");
+            }
+            else
+            {
+                isTurnOn = false;
+                Console.WriteLine($"turning off {model}...");
+            }
+        }
+
+        public void AddProcessor(int processors)
+        {
+            numberOfProcessor = numberOfProcessor + processors;
+            Console.WriteLine($"{model} has got {processors} new processors. and now have {numberOfProcessor} processors");
+        }
+
+        public override string ToString()
+        {
+            return $"Computer's model:{model}; price:{price}; numberOfProcessor:{numberOfProcessor};" +
+                $" screenSize:{screenSize}; isTurnOn:{isTurnOn}";
+        }
+    }
+}
---
+++ HWLesson13/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace HWLesson13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer Acer = new Computer()
+            {
+                model = "Acer's model",
+                price = 3890,
+                numberOfProcessor = 5,
+                screenSize = 14,
+                isTurnOn = true
+             };
+
+            
+            Console.WriteLine(Acer);
+            Acer.turnOn();
+            Console.WriteLine(Acer.tellMeThePrice());
+
+            Computer Asus = new Computer() {
+                model = "Asus's model",
+                price = 2979,
+                numberOfProcessor = 3,
+                screenSize = 15.6f,
+                isTurnOn = true,
+            };
+
+            Console.WriteLine(Asus);
+            Asus.turnOn();
+            Console.WriteLine(Asus.tellMeTheScreenSize());
+
+            Computer HP = new Computer() {
+                model = "HP's model",
+                price = 3474,
+                numberOfProcessor = 5,
+                screenSize = 14,
+                isTurnOn = false
+            };
+
+            Console.WriteLine(HP);
+            HP.AddProcessor(7);
+            HP.turnOff();
+
+        }
+    }
+}
